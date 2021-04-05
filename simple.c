f8f7eb544929b848411078961d46ebebaf0e8943
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2021-04-05 14:13:15 -0600

    Ejercicios 1 y 2

---
+++ simple.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/list.>
+
+int simple_init(void) {
+	printk(KERN_INFO "Loading Module\nLOLITO");
+	return 0;
+}
+void simple_exit(void) {
+	printk(KERN_INFO "Removing Module\nLOLITO")
+}
+module_init(simple_init);
+module_exit(simple_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Mi Perro se llama Lolito");
+MODULE_AUTHOR("Ricardo Valenzuela");
